dda88c635d86865b42196df189bd1d43b8a9e21e
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-02-16 19:09:15 +0530

    Rename SmartCollegeApplication file to match casing

--- src/main/java/com/example/smartCollege/SmartCollegeApplication.java
+++ src/main/java/com/example/smartCollege/SmartCollegeApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
-public class SmartCollege2Application {
+public class SmartCollegeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartCollege2Application.class, args);
 	}
 
 }
+
