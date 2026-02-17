725fae49334fd4e765a32e3484f4b56c9d802e0f
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-02-17 17:58:05 +0530

    Optional

--- src/main/java/com/example/smartCollege/StudentRepository.java
+++ src/main/java/com/example/smartCollege/StudentRepository.java
@@ -3,6 +3,7 @@ package com.smartCollege;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     // Custom method to find a student by email for login
