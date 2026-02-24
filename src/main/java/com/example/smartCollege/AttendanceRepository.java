462f8ad52f903abf07f27c71d7a60421eeb9a0b5
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-02-24 13:48:53 +0530

    attendance

--- src/main/java/com/example/smartCollege/AttendanceRepository.java
+++ src/main/java/com/example/smartCollege/AttendanceRepository.java
@@ -3,6 +3,7 @@ package com.smartCollege;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,6 +14,10 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long>{
     // For Daily/Monthly Reports
     List<Attendance> findByDateBetween(LocalDate start, LocalDate end);
     List<Attendance> findByDate(LocalDate date);
+	// This fixes the "findByStudentStudIdOrderByDateDesc" error
+    List<Attendance> findByStudentStudIdOrderByDateDesc(Long studId);
+
+	boolean existsByStudentStudIdAndDate(Long studId, LocalDate date);
     
     @Query("SELECT a FROM Attendance a LEFT JOIN FETCH a.student")
     List<Attendance> findAllWithStudents();
