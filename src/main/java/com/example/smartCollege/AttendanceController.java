2ba4ab176b9caf2d5d147e37c67e5fbfab64e2be
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-02-25 12:53:47 +0530

    updated

--- src/main/java/com/example/smartCollege/AttendanceController.java
+++ src/main/java/com/example/smartCollege/AttendanceController.java
@@ -38,8 +38,8 @@ public class AttendanceController {
     
  // --- TEMPORARY TESTING CONFIGURATION ---
  // Replace these with your current home/office coordinates
- private final double COLLEGE_LAT = 19.2099; // Your current Lat
- private final double COLLEGE_LON = 74.0431; // Your current Lon
+ private final double COLLEGE_LAT = 19.129892; // Your current Lat
+ private final double COLLEGE_LON = 74.190626; // Your current Lon
  private final double MAX_DISTANCE_METERS = 5000.0; // Keep it tight for testing
  
  @PostMapping("/mark/{studId}")
